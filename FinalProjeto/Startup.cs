8e242e5458203c273b5abc5392f3a3fa421e4c0d
Hector <hectorgatsos@gmail.com>
2020-07-01 18:29:17 +0300

    Add project files.

---
+++ FinalProjeto/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinalProjeto.Startup))]
+namespace FinalProjeto
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
